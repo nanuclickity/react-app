78d06db1273cb626408cbb4fadc216fb9abfe0d7
Tushar Kant <nanu.clickity@gmail.com>
2018-03-12 22:13:33 +0530

    exposed facebook reducer

--- src/client/store/rootReducer.js
+++ src/client/store/rootReducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
+import Facebook from './Facebook'
+
 export default combineReducers({
-  router: routerReducer
+  router: routerReducer,
+  Facebook
 })
