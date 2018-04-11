20e819a9da45b6585f3fcfab2d2abec9b99e60f8
Tushar Kant <nanu.clickity@gmail.com>
2018-04-11 16:04:31 +0530

    <TreeItem />

---
+++ src/client/components/ui/TreeView/TreeItem.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+
+export default class TreeItem extends Component {
+  state = {
+    isOpen: false
+  }
+
+  toggleOpen = () => {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  render() {
+    const { id, title, subtitle, children, className, onItemClick } = this.props
+
+    const childCount = React.Children.count(children)
+
+    const rootCx = classnames('tree-leaf', className, {
+      'is-open': this.state.isOpen,
+      'without-child': !childCount
+    })
+    const childrenCx = classnames('tree-leaf-children', {
+      'is-open': this.state.isOpen
+    })
+
+    return (
+      <div className={rootCx} data-id={id} onClick={e => onItemClick(e)}>
+        <div className="tree-leaf-inner">
+          {!!childCount && (
+            <div className="tree-leaf-toggle" onClick={this.toggleOpen}>
+              {this.state.isOpen ? ' - ' : ' + '}
+            </div>
+          )}
+          <div className="leaf-image" />
+          <div className="leaf-details">
+            <div className="leaf-title">{title}</div>
+            <div classname="leaf-subtitle">{subtitle}</div>
+          </div>
+        </div>
+        {!!children && <div className={childrenCx}>{children}</div>}
+      </div>
+    )
+  }
+}
+
+TreeItem.propTypes = {
+  // report an item click
+  onItemClick: PropTypes.func,
+
+  // [TODO] decide on only one type later on
+  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+
+  // name to display
+  name: PropTypes.string.isRequired,
+
+  // additional classname to support composition outside tree component if required
+  className: PropTypes.string,
+
+  children: PropTypes.node,
+
+  isOpen: PropTypes.bool
+}
